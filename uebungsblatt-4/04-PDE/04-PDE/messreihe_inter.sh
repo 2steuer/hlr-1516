f3e26f2042b032676ff8db185b3e39899e07a33b
4koglin <4koglin@informatik.uni-hamburg.de>
2015-11-05 20:32:42 +0100

    _inter added

--- uebungsblatt-4/04-PDE/04-PDE/messreihe_inter.sh
+++ uebungsblatt-4/04-PDE/04-PDE/messreihe_inter.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-./run_partdiff.sh 1 $1
-./run_partdiff.sh 2 $1
-./run_partdiff.sh 4 $1
-./run_partdiff.sh 8 $1
-./run_partdiff.sh 16 $1
-./run_partdiff.sh 32 $1
-./run_partdiff.sh 64 $1
-./run_partdiff.sh 128 $1
-#./run_partdiff.sh 256 $1
-#./run_partdiff.sh 512 $1
-#./run_partdiff.sh 1024 $1
+./run_partdiff_inter.sh 1 $1
+./run_partdiff_inter.sh 2 $1
+./run_partdiff_inter.sh 4 $1
+./run_partdiff_inter.sh 8 $1
+./run_partdiff_inter.sh 16 $1
+./run_partdiff_inter.sh 32 $1
+./run_partdiff_inter.sh 64 $1
+./run_partdiff_inter.sh 128 $1
+#./run_partdiff_inter.sh 256 $1
+#./run_partdiff_inter.sh 512 $1
+#./run_partdiff_inter.sh 1024 $1
 
