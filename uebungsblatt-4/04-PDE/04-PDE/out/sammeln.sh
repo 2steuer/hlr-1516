058119a4f2b9291ba00a20c90e1359d646ad305e
4koglin <koglin1@wr.informatik.uni-hamburg.de>
2015-11-06 17:48:36 +0100

    script zum sammeln der ergenisse

---
+++ uebungsblatt-4/04-PDE/04-PDE/out/sammeln.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -f "ergebnisse" ]
+then 
+	touche ergebnisse
+fi
+
+for ((i=7;i<11;i++)); do
+	for ((j=1;j<13;j++)); do
+		echo "run: $i threads: $j" >> ergebnisse;
+		cat t_${i}_${j}.out | grep zeit >> ergebnisse;
+	done
+done
