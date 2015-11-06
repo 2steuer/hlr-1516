bbf0065874b8644671069d4c95702e1e82792c1d
4koglin <4koglin@users.noreply.github.com>
2015-11-06 18:14:43 +0100

    Update sammeln.sh

--- uebungsblatt-4/04-PDE/04-PDE/out/sammeln.sh
+++ uebungsblatt-4/04-PDE/04-PDE/out/sammeln.sh
@@ -10,7 +10,7 @@ fi
 
 for ((i=1;i<10;i++)); do
 	for ((j=1;j<13;j++)); do
-		if [ ! -f "t_${i}_${j}.out" ]
+		if [ -f "t_${i}_${j}.out" ]
 			then 
 			echo "run: $i threads: $j" >> ergebnisse;
 			cat t_${i}_${j}.out | grep zeit >> ergebnisse;
