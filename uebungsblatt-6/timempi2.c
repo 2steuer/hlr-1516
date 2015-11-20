1a6acfa7ee9106e067426206d52e26122309605b
4koglin <4koglin@informatik.uni-hamburg.de>
2015-11-20 16:58:56 +0100

    master wieder mit in die ausgabe

--- uebungsblatt-6/timempi2.c
+++ uebungsblatt-6/timempi2.c
@@ -12,7 +12,7 @@
 int
 main()
 {
-	int  numtasks, taskid, source, i, tag;
+	int  numtasks, taskid, i, tag;
 	int global_microsec_min;
 	//char hostname[MPI_MAX_PROCESSOR_NAME];
 	MPI_Status status;
@@ -24,55 +24,55 @@ main()
 	MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
 
 	
-    if (taskid == MASTER)
-    {
-    	char formatted_string[40];
-        //wait for results from other tasks
-        for (i=0; i<numtasks; i++)
-        {
-            source = i;
-            //MPI_RECV(buf,count,datatype,source,tag,comm,status)
-            MPI_Recv(formatted_string, 40, MPI_CHAR, source, tag,  MPI_COMM_WORLD, &status);
+	char buffer_time[20];
+	char buffer_hostname[40];
+	char buffer_microsec[6];
+	int microsec;
+	struct tm* tm;
 
-            printf("%s\n", formatted_string);
-        }
+	struct timeval tv;
 
-        MPI_Reduce(&microsec, &global_microsec_min, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
-        printf("%d\n", global_microsec_min);
-    }
+	gettimeofday(&tv, NULL);
+
+	microsec = tv.tv_usec;
+
+	tm = localtime(&tv.tv_sec);
+	snprintf(buffer_microsec, 6, "%06d", microsec);
 
 
-	// We need to do this on EVERY process
+	strftime(buffer_time, 20, "%Y-%m-%d %H:%M:%S", tm);
+
+	gethostname(buffer_hostname, sizeof buffer_hostname);
+	strcat(buffer_hostname, ": ");
+	strcat(buffer_hostname, buffer_time);
+	strcat(buffer_hostname,".");
+	strcat(buffer_hostname, buffer_microsec);
+
 	if(taskid > MASTER)
 	{
-		char buffer_time[20];
-		char buffer_hostname[40];
-		char buffer_microsec[6];
-		int microsec;
-		struct tm* tm;
-
- 	   	struct timeval tv;
-
-		gettimeofday(&tv, NULL);
-
-		microsec = tv.tv_usec;
-
- 		tm = localtime(&tv.tv_sec);
- 		snprintf(buffer_microsec, 6, "%06d", microsec);
-
-
-		strftime(buffer_time, 20, "%Y-%m-%d %H:%M:%S", tm);
-
-		gethostname(buffer_hostname, sizeof buffer_hostname);
-		strcat(buffer_hostname, ": ");
-		strcat(buffer_hostname, buffer_time);
-		strcat(buffer_hostname,".");
-		strcat(buffer_hostname, buffer_microsec);
-
 		MPI_Reduce(&microsec, &global_microsec_min, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
 		//MPI_SEND(buf,count,datatype,dest,tag,comm)
 		MPI_Send(buffer_hostname, 40, MPI_CHAR, MASTER, tag, MPI_COMM_WORLD);
 	}
+
+	if (taskid == MASTER)
+    {
+    	char formatted_string[40];
+
+    	//own Hostname : timestamp
+    	printf("%s\n", buffer_hostname);
+
+    	MPI_Reduce(&microsec, &global_microsec_min, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
+        //wait for results from other tasks
+        for (i=0; i<numtasks; i++)
+        {
+            //MPI_RECV(buf,count,datatype,source,tag,comm,status)
+            MPI_Recv(formatted_string, 40, MPI_CHAR, i, tag,  MPI_COMM_WORLD, &status);
+
+            printf("%s\n", formatted_string);
+        }  
+        printf("%d\n", global_microsec_min);
+    }    
 
 	MPI_Barrier(MPI_COMM_WORLD);
 	printf("Rang %i beendet jetzt!\n", taskid);
