eefd7fb09f54a1a9e947e4c8f700dc87f8ae2acd
4koglin <4koglin@informatik.uni-hamburg.de>
2015-12-03 22:32:00 +0100

    revert

--- uebungsblatt-8/08-displaymatrix/partdiff-par.c
+++ uebungsblatt-8/08-displaymatrix/partdiff-par.c
@@ -425,27 +425,30 @@ calculateJacobi (struct calculation_arguments const* arguments, struct calculati
 		m1 = m2;
 		m2 = i;
 		
-		//reduce maxresiduum, broadcast afterwards
-		MPI_Allreduce(&maxresiduum, &globalresiduum, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+		//reduce maxresiduum
+		MPI_Reduce(&maxresiduum, &globalresiduum, 1, MPI_DOUBLE, MPI_MAX, MASTER, MPI_COMM_WORLD);
 
-		
-		results->stat_iteration++;
-		results->stat_precision = globalresiduum;
-
-		/* check for stopping calculation, depending on termination method */
-		if (options->termination == TERM_PREC)
+		if(rank == MASTER)
 		{
-			if (globalresiduum < options->term_precision)
+			results->stat_iteration++;
+			results->stat_precision = globalresiduum;
+
+			/* check for stopping calculation, depending on termination method */
+			if (options->termination == TERM_PREC)
 			{
-				term_iteration = 0;
+				if (globalresiduum < options->term_precision)
+				{
+					term_iteration = 0;
+				}
+			}
+			else if (options->termination == TERM_ITER)
+			{
+				term_iteration--;
 			}
 		}
-		else if (options->termination == TERM_ITER)
-		{
-			term_iteration--;
-		}
-		
 
+		//Broadcast term_iteration
+		MPI_Bcast(&term_iteration, 1, MPI_INT, MASTER, MPI_COMM_WORLD);
 	}
 	
 	results->m = m2;
